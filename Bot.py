eb7a31d3b79b4d68f460c449de1598c711716744
Hans Kristian <hanskristianom@gmail.com>
2020-09-03 22:05:26 +0200

    Bare en start, men det fungerer.

--- Bot.py
+++ Bot.py
@@ -9,14 +9,23 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
-    print(discord.member.status)
-   # print(client.user(166164767701073921).status)
+    #Henter alle medlemene den har tilgang til.
+    members = client.get_all_members()
+    #For hver member i arrayet (noe med flere elementer) members hentet ovenfra.
+    for member in members:
+        #Hvis statusen per medlem er det samme som status online printer vi den personen.
+        if member.status == discord.Status.online:
+            print(member)
+
+
+# print(client.user(166164767701073921).status)
 
 @client.event
 async def on_message(message):
@@ -24,10 +33,4 @@ async def on_message(message):
     print(message.author.status)
 
 
-
-
 client.run(TOKEN)
-
-
-
-
