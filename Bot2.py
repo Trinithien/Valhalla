96ae088c62c73fcd9c0974968072e6f17cb557b8
Trinithien <trinithien@gmail.com>
2020-09-04 15:30:12 +0200

    Updates status when a message is sent works.

--- Bot2.py
+++ Bot2.py
@@ -30,13 +30,17 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    s.enter(5, 1, check_online, (s,))
-    s.run()
+    #s.enter(5, 1, check_online, (s,))
+    #s.run()
 
-
-# print(client.user(166164767701073921).status)
 @client.event
 async def on_message(message):
     print(message.author.id)
     print(message.author.status)
+    members = client.get_all_members()
+    # For hver member i arrayet (noe med flere elementer) members hentet ovenfra.
+    for member in members:
+        # Hvis statusen per medlem er det samme som status online printer vi den personen.
+        if member.status == discord.Status.online:
+            print(member)
 client.run(TOKEN)
